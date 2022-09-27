6ce776965a0c14fab27914ada69d435d2855326b
Ng Wei Han <47109095+weihan7742@users.noreply.github.com>
2022-09-27 16:33:36 +0800

    feat(color mode): enable dark and light mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
   <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode="light" />
     <Component {...pageProps} />
   </ChakraProvider>)
 }

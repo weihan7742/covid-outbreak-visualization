b734dc6ce2e70f6c7f0d293107ff6316f5aaf595
Ng Wei Han <47109095+weihan7742@users.noreply.github.com>
2022-09-27 14:57:36 +0800

    Set up repo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  // TODO: Change theme here!
+  const theme = extendTheme({
+    fonts: {
+      heading: 'Inter',
+      body: 'Inter',
+    }
+  });
+
+  return (
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>)
 }
 
 export default MyApp
